d8448ed2cb21b7d15ea7454a0688053f88948066
wangweilin <wangweilin@nonobank.com>
2018-07-24 10:22:47 +0800

    del

--- spring-cloud-server/spring-cloud-server1/src/main/java/com/xiamoan/springcloud/server1/TestController.java
+++ spring-cloud-server/spring-cloud-server1/src/main/java/com/xiamoan/springcloud/server1/TestController.java
@@ -1,5 +1,6 @@
 package com.xiamoan.springcloud.server1;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class TestController {
     private Server2Client server2Client;
 
     @RequestMapping("test")
+    @Hystrix
     public Object test(){
         return server2Client.test();
     }
